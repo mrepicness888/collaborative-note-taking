876ba85ca8477d5d6392ac329a733e6e7138288a
Michael Ha <150257321+mrepicness888@users.noreply.github.com>
2026-04-30 21:55:04 +0100

    clean code

--- src/helpers/generateColour.ts
+++ src/helpers/generateColour.ts
@@ -1,14 +1,15 @@
-  export const generateColour = (name: string) => {
-    const colours = [
-      "#1abc9c",
-      "#3498db",
-      "#9b59b6",
-      "#e67e22",
-      "#e74c3c",
-      "#f1c40f",
-    ];
-    let hash = 0;
-    for (let i = 0; i < name.length; i++)
-      hash = name.charCodeAt(i) + ((hash << 5) - hash);
-    return colours[Math.abs(hash) % colours.length];
-  };
+// This function generates a colour based on name, so that the same name will always get the same colour.
+export const generateColour = (name: string) => {
+  const colours = [
+    "#1abc9c",
+    "#3498db",
+    "#9b59b6",
+    "#e67e22",
+    "#e74c3c",
+    "#f1c40f",
+  ];
+  let hash = 0;
+  for (let i = 0; i < name.length; i++)
+    hash = name.charCodeAt(i) + ((hash << 5) - hash);
+  return colours[Math.abs(hash) % colours.length];
+};
